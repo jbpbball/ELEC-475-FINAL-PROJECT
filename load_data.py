003e493d7d162c5903cac25021d3e18f0511437a
Joshua <jbp3@rice.edu>
2018-04-10 02:52:38 -0500

    example of loading dataset

---
+++ load_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVR
+
+
+student_df = pd.read_csv("StudentData.csv")
+pertusis_df = pd.read_csv("pertusisRates2010_2015.csv")
+#print student_df.loc[np.where(np.logical_and(student_df['year'] == 2000, student_df['COUNTY'] == 'YUBA'))]
+counties =  student_df.COUNTY.unique()
+years = student_df.year.unique()
+
+features = np.zeros((len(counties), 3))
+p_rates = []
+
+for i, county in enumerate(counties):
+	if county == "CALIFORNIA":
+		continue
+	print county
+	n = student_df.loc[np.where(np.logical_and(student_df[
+	'year'] == 2009, student_df['COUNTY'] == county))]['n'].sum()
+	features[i,0] = student_df.loc[np.where(np.logical_and(student_df[
+	'year'] == 2009,student_df['COUNTY'] == county))]['nMMR'].sum()/n
+	features[i,1] = student_df.loc[np.where(np.logical_and(student_df[
+	'year'] == 2009, student_df['COUNTY'] == county))]['nDTP'].sum()/n
+	features[i,2] = student_df.loc[np.where(np.logical_and(student_df[
+	'year'] == 2009, student_df['COUNTY'] == county))]['nPolio'].sum()/n
+	
+	p_rates.append(pertusis_df.loc[i]['Rate2010'])
+	
+	
+clf = SVR()
+clf.fit(features,p_rates)
+
+print clf.score(features,p_rates)
